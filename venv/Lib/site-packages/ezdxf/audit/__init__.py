cdcab80ab3b6ab3aab87d1fe91ce6e9c910d0bfc
cancerak <maciej.spisak@protonmail.com>
2019-05-24 12:14:09 +0200

    unwanted files or some inline comments

--- venv/Lib/site-packages/ezdxf/audit/__init__.py
+++
@@ -1,6 +0,0 @@
-# Purpose: DXF audit package
-# Created: 10.03.2017
-# Copyright (C) 2017, Manfred Moitzi
-# License: MIT License
-from ezdxf.audit.auditor import Auditor
-from ezdxf.lldxf.const import Error
